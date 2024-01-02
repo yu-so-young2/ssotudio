7a4c3e07ae5d9957ae850be91b733e1d434c4d56
ssoyoung <sy980911@gmail.com>
2024-01-02 17:31:16 +0900

    feat : 하영님을 위한 테스트 히히

--- ssotudio/src/main/java/com/soyoung/ssotudio/controller/TestController.java
+++ ssotudio/src/main/java/com/soyoung/ssotudio/controller/TestController.java
@@ -2,6 +2,7 @@ package com.soyoung.ssotudio.controller;
 
 import com.soyoung.ssotudio.controller.response.BasicResponse;
 import com.soyoung.ssotudio.controller.response.ResultType;
+import com.soyoung.ssotudio.dto.response.EnumDto;
 import com.soyoung.ssotudio.exception.CustomException;
 import com.soyoung.ssotudio.exception.ExceptionType;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,6 +12,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Tag(name = "Test", description = "테스트 API")
@@ -33,6 +37,34 @@ public class TestController {
     public ResponseEntity<BasicResponse<String>> fail()  {
 
         throw new CustomException(ExceptionType.FORM_FIELD_TYPE_NOT_FOUND);
+    }
+
+    @GetMapping("/")
+    @Operation(summary = "테스트")
+    public ResponseEntity<BasicResponse<List<EnumDto>>> test(@RequestParam(required = false) String param1, @RequestParam(required = false) String param2) {
+        List<EnumDto> enumList = new ArrayList<>();
+
+        if("고기".equals(param1)) {
+            enumList.add(EnumDto.builder().label("삼겹살").value("삼겹살").build());
+            enumList.add(EnumDto.builder().label("갈비").value("갈비").build());
+        }
+        else if("밥".equals(param1)) {
+            enumList.add(EnumDto.builder().label("비빔밥").value("비빔밥").build());
+            enumList.add(EnumDto.builder().label("김치볶음밥").value("김치볶음밥").build());
+        }
+
+        if("매운거".equals(param2)) {
+            enumList.add(EnumDto.builder().label("떡볶이").value("떡볶이").build());
+            enumList.add(EnumDto.builder().label("마라탕").value("마라탕").build());
+        }
+        else if("안매운거".equals(param2)) {
+            enumList.add(EnumDto.builder().label("샤브샤브").value("샤브샤브").build());
+            enumList.add(EnumDto.builder().label("돌솥밥").value("돌솥밥").build());
+        }
+
+
+        BasicResponse<List<EnumDto>> response = BasicResponse.of(ResultType.SUCCESS, null, enumList);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
